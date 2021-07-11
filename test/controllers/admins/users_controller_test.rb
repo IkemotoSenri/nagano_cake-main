b9753e8f3a1884f4091402387150efface2d22b0
EC2 Default User <ec2-user@ip-172-31-15-248.ec2.internal>
2021-07-11 11:49:34 +0000

    Userログイン機能追加

--- test/controllers/admins/users_controller_test.rb
+++ test/controllers/admins/users_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CustomersControllerTest < ActionDispatch::IntegrationTest
+class Admins::UsersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
