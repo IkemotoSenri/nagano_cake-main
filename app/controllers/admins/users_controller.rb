654edec8c5761adf1e32d3f585c1a7e79580f412
EC2 Default User <ec2-user@ip-172-31-15-248.ec2.internal>
2021-07-15 10:00:04 +0000

    会員詳細

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,14 +1,18 @@
 class Admins::UsersController < ApplicationController
   def index
-    @users = Users.all
+     @users = User.all
   end
   
   def show
+     @user = User.find(params[:id])
   end
   
   def edit
   end
   
+  private 
   
-
+  def user_params
+     params.require(:user).permit(:email, :first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :is_active)
+  end
 end
